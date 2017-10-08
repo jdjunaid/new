2265ef9ea34dc8f4ceb31da19a14f2bd5b965142
jdjunaid <am.junaidali@gmail.com>
2017-10-08 17:09:26 +0500

    started with c++

---
+++ main (1).cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter no. for the table?";
+    cin >>x;
+    cout<<x<<" x 1 "<<"="<<x*1 <<endl;
+    cout<<x<<" x 2"<<"="<<x*2<<endl;
+    cout<<x<<" x 3"<<"="<<x*3<<endl;
+    cout<<x<<" x 4"<<"="<<x*4<<endl;
+    cout<<x<<" x 5"<<"="<<x*5<<endl;
+    cout<<x<<" x 6"<<"="<<x*6<<endl;
+    cout<<x<<" x 7"<<"="<<x*7<<endl;
+    cout<<x<<" x 8"<<"="<<x*8<<endl;
+    cout<<x<<" x 9"<<"="<<x*9<<endl;
+    cout<<x<<" x 10"<<"="<<x*10<<endl;
+    return 0;
+}
+
+
+
---
+++ main.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x ;
+    int y ;
+    cout<<"Enter your number?";
+    cin >> x;
+    cin >> y;
+    if(x>y)
+    {
+        cout<<x<<"is greater than"<<y;
+    }
+    if(x<y)
+    {
+        cout<<x<<"is less than"<<y;
+    }
+    cout<<"thank you";
+    return 0;
+}
+
+
+
